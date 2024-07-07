9eaf837eb6795ba16d2f4c0f3957c838a843e5ea
vini jai marsaline john peter <j.vinijaimarsaline@gamil.com>
2024-07-07 12:24:02 +0200

    Prod ready

--- azwebapp/azwebapp/wsgi.py
+++ azwebapp/azwebapp/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azwebapp.settings')
+settings_module = 'azwebapp.deployement' if 'WEBSITE_HOSTNAME' in os.environ else 'azwebapp.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
